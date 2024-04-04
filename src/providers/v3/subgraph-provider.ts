c1e0e7f7b1b538a27a0e3d2de729cb51fa2837af
norbubb <norbubb@proton.me>
2024-04-04 19:39:39 +0800

    change rpc

--- src/providers/v3/subgraph-provider.ts
+++ src/providers/v3/subgraph-provider.ts
@@ -67,7 +67,7 @@ const SUBGRAPH_URL_BY_CHAIN: { [chainId in ChainId]?: string } = {
     'https://api.thegraph.com/subgraphs/name/lynnshaoyu/uniswap-v3-avax',
   [ChainId.BASE]:
     'https://api.studio.thegraph.com/query/48211/uniswap-v3-base/version/latest',
-  [ChainId.X1_TESTNET]: "http://35.241.74.232:8000/subgraphs/name/jaguarswap/uniswap-v3",
+  [ChainId.X1_TESTNET]: "https://subgraph.jaguarex.com/subgraphs/name/jaguarswap/uniswap-v3",
   [ChainId.BLAST]: 'https://gateway-arbitrum.network.thegraph.com/api/0ae45f0bf40ae2e73119b44ccd755967/subgraphs/id/2LHovKznvo8YmKC9ZprPjsYAZDCc4K5q4AYz8s3cnQn1',
 };
 
